5c8db21edbdc330f80c998e861f52febff26b62d
Gourita-M <mouad.gourita@gmail.com>
2026-09-23 11:56:31 +0000

    Implement viewTransactions method to retrieve and display transactions by user email

--- src/Services/TransactionService.java
+++ src/Services/TransactionService.java
@@ -1,5 +1,26 @@
 package src.Services;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import src.DAO.TransactionDAO;
+
 public class TransactionService {
     
+    public static void viewTransactions(String userEmail){
+
+    try(ResultSet rs = TransactionDAO.getTransactionByUserEmail(userEmail)){
+
+        while(rs.next()){
+            Date date = rs.getDate("date");
+            int amount = rs.getInt("amount");
+            String type = rs.getString("type");
+            String location = rs.getString("location");
+            System.out.println(type + " | " + amount + "DH " + date + " " + location);
+        }
+
+    }catch(SQLException e){
+        System.out.println(e.getMessage());
+    }
+    }
 }
