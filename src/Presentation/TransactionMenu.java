afb0e149ce03eccc5bf56929cbcdd6ddfc17046a
Gourita-M <mouad.gourita@gmail.com>
2026-09-23 11:08:37 +0000

    Add TransactionMenu class for handling transaction-related user input

---
+++ src/Presentation/TransactionMenu.java
@@ -0,0 +1,15 @@
+package src.Presentation;
+
+import java.util.Scanner;
+
+public class TransactionMenu {
+    private static Scanner scan = new Scanner(System.in);
+    
+    public static void show()
+    {
+        System.out.print("Enter Account Email: ");
+        //String Account = scan.nextLine();
+
+        
+    }
+}
