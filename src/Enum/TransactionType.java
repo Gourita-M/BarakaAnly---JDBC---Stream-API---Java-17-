b4c3fd4eff9528d081c5e7a42231c7cc438abf08
Gourita-M <mouad.gourita@gmail.com>
2026-09-23 11:08:15 +0000

    Add TransactionType enum, SavingsAccount class, and Transaction class with properties and methods

---
+++ src/Enum/TransactionType.java
@@ -0,0 +1,7 @@
+package src.Enum;
+
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAWAL,
+    TRANSFER
+}
---
+++ src/entity/Transaction.java
@@ -0,0 +1,70 @@
+package src.entity;
+
+import java.util.Date;
+import src.Enum.TransactionType;
+
+public class Transaction {
+    private int id;
+    private Date date;
+    private Double amount;
+    private TransactionType type;
+    private String location;
+    private int accountId;
+
+    public Transaction(int id, Date date, Double amount, TransactionType type, String location, int accountId) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.type = type;
+        this.location = location;
+        this.accountId = accountId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+}
