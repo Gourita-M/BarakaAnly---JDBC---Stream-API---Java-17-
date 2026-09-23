215a2c5044adf16a13005036273871899bbaa3e5
Gourita-M <mouad.gourita@gmail.com>
2026-09-23 11:56:10 +0000

    Refactor TransactionDAO to retrieve transactions by user email instead of user ID

--- src/DAO/TransactionDAO.java
+++ src/DAO/TransactionDAO.java
@@ -1,20 +1,47 @@
 package src.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import src.database.DB;
 
 public class TransactionDAO {
     
-    public void getTransactionByUserId(int userId)
+    public static ResultSet getTransactionByUserEmail(String userEmail)
     {
         try {
-            
             Connection connection = DB.connection();
-            String sql = "SELECT * FROM Transaction as t WHERE t.id = 1";
+
+            String sql = "SELECT u.id FROM Client as u WHERE u.email = ?";
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setString(1, userEmail);
+
+            ResultSet result = ps.executeQuery();
+
+            int userId;
+
+            if(result.next()){
+                userId = result.getInt("id");
+            }else{
+                return null;
+            }
+
+            sql = "SELECT * FROM Transaction as t WHERE t.accountId = ?";
+
+            ps = connection.prepareStatement(sql);
+
+            ps.setInt(1, userId);
+
+            result = ps.executeQuery();
+
+            return result;
 
         } catch (SQLException e) {
             System.out.println(e);
+            return null;
         }
         
 
