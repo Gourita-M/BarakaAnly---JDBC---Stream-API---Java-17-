30007510a0700c4ef65f53ed8a59f3481c56978f
Gourita-M <mouad.gourita@gmail.com>
2026-09-23 11:07:56 +0000

    Add DB class for database connection management

---
+++ src/database/DB.java
@@ -0,0 +1,19 @@
+package src.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+    public static Connection connection() throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/Testing";
+        String userName = "root";
+        String password = "";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+
+        return connection;
+    }
+}
