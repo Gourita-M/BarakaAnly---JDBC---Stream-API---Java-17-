82dadd3398c1d0bad0e98d987ba9a042afa2ad4d
Gourita-M <mouad.gourita@gmail.com>
2026-09-24 14:18:07 +0000

    Add Menu class to display user options for banking transactions

---
+++ src/UI/Menu.java
@@ -0,0 +1,42 @@
+package src.UI;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private static Scanner scan = new Scanner(System.in); 
+
+    public static void show()
+    {
+        boolean picked = false;
+        int choice = 100;
+
+        while(!picked){
+            System.out.println("********** Welcome to Bank Analys **********");
+            System.out.println("1. Record a transaction (deposit, withdrawal, transfer)");
+            System.out.println("2. View the transaction history of an account");
+            System.out.println("3. Analysis");
+            System.out.println("4. Alerts");
+            System.out.println("0. Exit");
+            System.out.print("Enter Your Choice: ");
+            choice = scan.nextInt();
+
+            switch (choice) {
+                case 0:
+                    picked = true;
+                    break;
+                case 1:
+                    Record.show();
+                    break;
+                case 2:
+                    TransactionMenu.show();
+                    break;
+                default:
+                    System.out.println("Invalid Choice. Try Again");
+            }
+            
+        }
+        
+
+    }
+}
