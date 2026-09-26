658d3274079abf3682d1cc47d1c07eedb9fbadf0
Gourita-M <mouad.gourita@gmail.com>
2026-09-26 15:16:53 +0000

    feat: Update deposit, withdrawal, and transfer methods to accept parameters for amount and accountId

--- src/Services/TransactionService.java
+++ src/Services/TransactionService.java
@@ -4,12 +4,36 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import src.DAO.TransactionDAO;
+import src.Enum.*;
+import src.entity.Transaction;
 
 public class TransactionService {
 
-    public static void deposit()
+    public static void deposit(Double amount, int accountId)
     {
-        
+        Date date = new Date();
+        Transaction transaction = new Transaction(date, amount,TransactionType.DEPOSIT, "SAFI", accountId);
+        if(!TransactionDAO.createTransaction(transaction)){
+            System.out.println("Error Adding a Transaction");
+        }
+    }
+
+    public static void withdrawal(Double amount, int accountId)
+    {
+        Date date = new Date();
+        Transaction transaction = new Transaction(date, amount,TransactionType.WITHDRAWAL, "SAFI", accountId);
+        if(!TransactionDAO.createTransaction(transaction)){
+            System.out.println("Error Adding a Transaction");
+        }
+    }
+
+    public static void transfer(Double amount, int accountId)
+    {
+        Date date = new Date();
+        Transaction transaction = new Transaction(date, amount,TransactionType.TRANSFER, "SAFI", accountId);
+        if(!TransactionDAO.createTransaction(transaction)){
+            System.out.println("Error Adding a Transaction");
+        }
     }
     
     public static void viewTransactions(String userEmail){
